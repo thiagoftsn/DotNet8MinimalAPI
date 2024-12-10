4140f0eeea6b5513123cfffe7e981c8dbe2729ba
Thiago Nascimento <thiagoftsn@hotmail.com>
2024-12-10 22:44:27 +0000

    Added .NET 8 Minimal API with core functionality

---
+++ DotNet8MinimalAPI/DotNet8MinimalAPI/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8MinimalAPI
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
